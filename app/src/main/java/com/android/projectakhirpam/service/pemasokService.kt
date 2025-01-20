6132999796610e4787bbc7e2a6d06ee8249efefe
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2025-01-20 14:03:04 +0700

    membuat pemasokService

---
+++ app/src/main/java/com/android/projectakhirpam/service/pemasokService.kt
@@ -0,0 +1,33 @@
+package com.android.projectakhirpam.service
+
+import com.android.projectakhirpam.model.*
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface pemasokService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+
+    @POST("store")
+    suspend fun insertPemasok(@Body pemasok: Pemasok)
+
+    @GET(".")
+    suspend fun getAllPemasok(): AllPemasokResponse
+
+    @GET("{id}")
+    suspend fun getPemasokById(@Path("id") idPemasok: Int): PemasokDetailResponse
+
+    @PUT("{id}")
+    suspend fun updatePemasok(@Path("id") idPemasok: Int, @Body pemasok: Pemasok)
+
+    @DELETE("{id}")
+    suspend fun deletePemasok(@Path("id") idPemasok: Int): Response<Void>
+}
