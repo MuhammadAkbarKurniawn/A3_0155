3dcc69a1864dc9f2f748fb72ead58ea66bab6c6e
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2025-01-24 20:26:26 +0700

    inisialisasi viewmodel

--- app/src/main/java/com/android/projectakhirpam/ui/viewmodel/penyediaViewModel.kt
+++ app/src/main/java/com/android/projectakhirpam/ui/viewmodel/penyediaViewModel.kt
@@ -10,6 +10,10 @@ import com.android.projectakhirpam.ui.viewmodel.kategori.DetailKategoriViewModel
 import com.android.projectakhirpam.ui.viewmodel.kategori.KategoriInsertViewModel
 import com.android.projectakhirpam.ui.viewmodel.kategori.KategoriUpdateViewModel
 import com.android.projectakhirpam.ui.viewmodel.kategori.KategoriViewModel
+import com.android.projectakhirpam.ui.viewmodel.pemasok.DetailPemasokViewModel
+import com.android.projectakhirpam.ui.viewmodel.pemasok.PemasokInsertViewModel
+import com.android.projectakhirpam.ui.viewmodel.pemasok.PemasokUpdateViewModel
+import com.android.projectakhirpam.ui.viewmodel.pemasok.PemasokViewModel
 import com.android.projectakhirpam.ui.viewmodel.produk.DetailViewModel
 import com.android.projectakhirpam.ui.viewmodel.produk.HomeViewModel
 import com.android.projectakhirpam.ui.viewmodel.produk.InsertViewModel
@@ -26,6 +30,11 @@ object penyediaViewModel {
         initializer { KategoriInsertViewModel(aplikasiProduk().container.kategoriRepository) }
         initializer { DetailKategoriViewModel(createSavedStateHandle(),aplikasiProduk().container.kategoriRepository) }
         initializer { KategoriUpdateViewModel(createSavedStateHandle(),aplikasiProduk().container.kategoriRepository) }
+
+        initializer { PemasokViewModel(aplikasiProduk().container.pemasokRepository) }
+        initializer { PemasokInsertViewModel(aplikasiProduk().container.pemasokRepository) }
+        initializer { DetailPemasokViewModel(createSavedStateHandle(),aplikasiProduk().container.pemasokRepository) }
+        initializer { PemasokUpdateViewModel(createSavedStateHandle(),aplikasiProduk().container.pemasokRepository) }
     }
 
     fun CreationExtras.aplikasiProduk(): ProdukApplications =
